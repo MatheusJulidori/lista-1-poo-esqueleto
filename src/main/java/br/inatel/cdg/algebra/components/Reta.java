e08d7a4e09a78cf6111aaf1250fa6d85b1e21ac9
Matheus <matheus_julidori@yahoo.com.br>
2021-08-31 14:12:22 -0300

    Final Project, with compiling error

---
+++ src/main/java/br/inatel/cdg/algebra/components/Reta.java
@@ -0,0 +1,33 @@
+package br.inatel.cdg.algebra.components;
+
+public class Reta {
+
+    private Ponto p1;
+    private Ponto p2;
+
+
+    public void setPoints(double p1x, double p1y, double p2x, double p2y){
+        p1.setX(p1x);
+        p2.setX(p2x);
+        p1.setY(p1y);
+        p2.setY(p2y);
+    }
+
+    public double angularCoef(){
+        double p2y = p2.getY();
+        double p2x = p2.getX();
+        double p1y = p1.getY();
+        double p1x = p1.getX();
+
+        return (p2y-p1y)/(p2x-p1x);
+    }
+
+    public double linearCoef() {
+        double p1y = p1.getY();
+        double p1x = p1.getX();
+        double ca = angularCoef();
+
+        return p1y - (ca*p1x);
+    }
+
+}
--- src/main/java/br/inatel/cdg/algebra/scene/ScenePrincipal.java
+++ src/main/java/br/inatel/cdg/algebra/scene/ScenePrincipal.java
@@ -1,5 +1,6 @@
 package br.inatel.cdg.algebra.scene;
 
+import br.inatel.cdg.algebra.components.Reta;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -8,39 +9,85 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
 public class ScenePrincipal {
 
-    private Button btnTeste, btnCalcCoefLinear; //Button representa botoes
-    private Label labelTeste1; //Label representam rótulos
-    private TextField textField1, textField2; //TextField Representam áreas de texto
-
+    private Button btnCalcCoefAngular, btnCalcCoefLinear; //Button representa botoes
+    private Label labelP1x,labelP1y,labelP2x,labelP2y,labelCoefAng,labelCoefLin; //Label representam rótulos
+    private TextField textFieldP1x, textFieldP2x,textFieldP1y,textFieldP2y,textFieldCoefLin,textFieldCoefAng; //TextField Representam áreas de texto
+    private Reta reta = new Reta();
     public void criaScenePrincipal(Stage stage){
 
-        //Criando os labels para os pontos e os campos de texto para os dados
-        labelTeste1 = new Label("Digite alguma coisa"); //rótulos
-        textField1 = new TextField(); //área de texto onde vc digitara alguma coisa
 
+        //----------Area de texto P1.x---------------------------
+        //Criando os labels para os pontos e os campos de texto para os dados
+        labelP1x = new Label("Ponto P1.X"); //rótulos
+        textFieldP1x = new TextField(); //área de texto onde vc digitara alguma coisa
 
         //HBox é usado para agrupar elementos horizontalmente
-        HBox grupoHorizontal = new HBox(labelTeste1, textField1); //Passamos no construtor todos os elementos. Você poderá criar vários grupos horizontais
+        HBox horizontalP1x = new HBox(labelP1x, textFieldP1x); //Passamos no construtor todos os elementos. Você poderá criar vários grupos horizontais
 
 
+        //----------Area de texto P1.y---------------------------
+        labelP1y = new Label("Ponto P1.Y");
+        textFieldP1y = new TextField();
+        HBox horizontalP1y = new HBox(labelP1y, textFieldP1y);
+
+        //----------Area de texto P2.x---------------------------
+        labelP2x = new Label("Ponto P2.X");
+        textFieldP2x = new TextField();
+        HBox horizontalP2x = new HBox(labelP2x, textFieldP2x);
+
+        //----------Area de texto P2.y---------------------------
+        labelP2y = new Label("Ponto P2.Y");
+        textFieldP2y = new TextField();
+        HBox horizontalP2y = new HBox(labelP2y, textFieldP2y);
+
+
+        //----------Area de texto coeficiente angular---------------------------
         //Agora vamos criar a area que mostrará o que foi digitado
-        textField2 = new TextField();
-        textField2.setEditable(false);//vamos deixar false para apenas mostrar texto
-        textField2.setText("Sua frase aparecerá aqui");
+        textFieldCoefAng = new TextField();
+        textFieldCoefAng.setEditable(false);//vamos deixar false para apenas mostrar texto
+        textFieldCoefAng.setText("Coeficiente Angular: ");
 
+
+        //----------Area de texto coeficiente linear---------------------------
+        textFieldCoefLin = new TextField();
+        textFieldCoefLin.setEditable(false);
+        textFieldCoefLin.setText("Coeficiente Linear: ");
+        HBox horizontalResults = new HBox(textFieldCoefAng, textFieldCoefLin);
+
+
+        //----------Area de botao coeficiente angular---------------------------
         //Criamos o botão
-        btnTeste = new Button("Executar Ação");
+        btnCalcCoefAngular = new Button("Calcula coeficiente angular");
         //Criamos a ação que o botão responderá as ser pressionado
-        btnTeste.setOnAction(evento -> {
-            //Aqui dentro é a ação que será executado ao pressionar o botão
-            textField2.setText(textField1.getText());//Acessamos o componente textField1, pegamos o texto e colocaos em textField2
+        btnCalcCoefAngular.setOnAction(evento -> {
+            double p1x = Double.parseDouble(textFieldP1x.getText());
+            double p1y = Double.parseDouble(textFieldP1y.getText());
+            double p2x = Double.parseDouble(textFieldP2x.getText());
+            double p2y = Double.parseDouble(textFieldP2y.getText());
+            reta.setPoints(p1x,p1y,p2x,p2y);
+
+            textFieldCoefAng.setText(String.valueOf(reta.angularCoef()));//Acessamos o componente textField1, pegamos o texto e colocaos em textField2
         });
+
+        //----------Area de botao coeficiente Linear---------------------------
+        btnCalcCoefLinear = new Button("Calcula coeficiente linear");
+        btnCalcCoefLinear.setOnAction(evento -> {
+            double p1x = Double.parseDouble(textFieldP1x.getText());
+            double p1y = Double.parseDouble(textFieldP1y.getText());
+            double p2x = Double.parseDouble(textFieldP2x.getText());
+            double p2y = Double.parseDouble(textFieldP2y.getText());
+            reta.setPoints(p1x,p1y,p2x,p2y);
+
+            textFieldCoefLin.setText(String.valueOf(reta.linearCoef()));//Acessamos o componente textField1, pegamos o texto e colocaos em textField2
+        });
+        HBox horizontalButtons = new HBox(btnCalcCoefAngular, btnCalcCoefLinear);
 
         //VBox é usada para agrupar elementos verticalmente
         //No construtor passamos todos os elementos que serão agrupados, que podem ser outros grupos
-        VBox layoutFinal = new VBox(grupoHorizontal, textField2,btnTeste);//Aqui vamos agrupar verticalmente o grupo (Label + Texto) o Botao e A area que aparecer o texto digitado
+        VBox layoutFinal = new VBox(horizontalP1x, horizontalP1y,horizontalP2x,horizontalP2y,horizontalResults,horizontalButtons);//Aqui vamos agrupar verticalmente o grupo (Label + Texto) o Botao e A area que aparecer o texto digitado
         //Criamos a Scene
         Scene scene = new Scene(layoutFinal, 300 , 200);
 
